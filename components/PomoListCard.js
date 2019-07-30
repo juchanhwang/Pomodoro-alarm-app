b7174ca6e1558657a057720df52cd3733d7d8dfe
juchanhwang <ghkdwncks96@gmail.com>
2019-07-30 15:20:17 +0900

    refactoring: edit styled-componet to styleSheet

--- components/PomoListCard.js
+++ components/PomoListCard.js
@@ -1,54 +1,51 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
-const Container = styled.View`
-  width: 312;
-  height: 77;
-  flexDirection: row;
-  justifyContent: center;
-  borderRadius: 16;
-  backgroundColor: #666666;
-`;
-
-const CardContainer = styled.View`
-  width: 224;
-  justifyContent: center;
-`;
-
-const CardCategory = styled.Text`
-  height: 14;
-  opacity: 0.5;
-  fontSize: 12;
-  fontWeight: 300;
-  fontStyle: normal;
-  letterSpacing: -0.3;
-  color: #ffffff;
-`;
-
-const CardPomoName = styled.Text`
-  height: 19;
-  fontSize: 16;
-  fontWeight: 500;
-  fontStyle: normal;
-  letterSpacing: -0.5;
-  color: #ffffff;
-`;
-
-const PageContainer = styled.View`
-  justifyContent: center;
-`;
-
-const CurrentPage = styled.Text`
-  width: 20;
-  height: 14;
-  fontSize: 12;
-  fontWeight: normal;
-  fontStyle: normal;
-  letterSpacing: -0.38;
-  textAlign: right;
-  color: #ffffff;
-`;
+const styles = StyleSheet.create({
+  container: {
+    width: 312,
+    height: 77,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    borderRadius: 16,
+    backgroundColor: '#666666',
+  },
+  cardContainer: {
+    width: 224,
+    justifyContent: 'center',
+  },
+  cardCategory: {
+    height: 14,
+    opacity: 0.5,
+    fontSize: 12,
+    fontWeight: '300',
+    fontStyle: 'normal',
+    letterSpacing: -0.3,
+    color: '#ffffff',
+  },
+  cardPomoName: {
+    height: 19,
+    fontSize: 16,
+    fontWeight: '500',
+    fontStyle: 'normal',
+    letterSpacing: -0.5,
+    color: '#ffffff',
+  },
+  pageContainer: {
+    justifyContent: 'center',
+  },
+  currentPage: {
+    width: 20,
+    height: 14,
+    fontSize: 12,
+    fontWeight: 'normal',
+    fontStyle: 'normal',
+    letterSpacing: -0.38,
+    textAlign: 'right',
+    color: '#ffffff',
+  },
+});
 
 export default class ContentCard extends Component {
   static defaultProps = {
@@ -60,17 +57,17 @@ export default class ContentCard extends Component {
 
   render() {
     return (
-      <Container>
-        <CardContainer>
-          <CardCategory> {this.props.category} </CardCategory>
-          <CardPomoName> {this.props.pomoName} </CardPomoName>
-        </CardContainer>
-        <PageContainer>
-          <CurrentPage>
+      <View style={styles.container}>
+        <View style={styles.cardContainer}>
+          <Text style={styles.cardCategory}> {this.props.category} </Text>
+          <Text style={styles.cardPomoName}> {this.props.pomoName} </Text>
+        </View>
+        <View style={styles.pageContainer}>
+          <Text style={styles.currentPage}>
             {this.props.CurPage}/{this.props.totalPage}
-          </CurrentPage>
-        </PageContainer>
-      </Container>
+          </Text>
+        </View>
+      </View>
     );
   }
 }
